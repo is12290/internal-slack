4d26803992a0953aea150ad0f3f2d64a285500b4
Ian Scalzo <scalzo.ian@gmail.com>
2019-05-18 10:47:16 -0400

    i don't think this will work

--- skills/results.js
+++ skills/results.js
@@ -1,114 +1,52 @@
 module.exports = function(controller) {
 
     controller.hears(['result', 'Result', 'results', 'Results'], 'direct_message,direct_mention', function(bot, message) {
-        
-        var average = getAverage(message);
-        var percent = getPercentage(message);
 
-
-        // Block Content
-        const content = {
-            blocks: [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Hey! Here are your results...\n\n*Sleep*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#02D2FF",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[0] + "\n*Negative:* " + percent[1] + "\n" + average[0] + "\n\n*Energy*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#2A02FF",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[2] + "\n*Negative:* " + percent[3] + "\n" + average[1] + "\n\n*Mood*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#8A02FF",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[4] + "\n*Negative:* " + percent[5] + "\n" + average[2] + "\n\n*Motivation*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#CF02FF",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[6] + "\n*Negative:* " + percent[7] + "\n" + average[3] + "\n\n*Efficiency*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#FF029D",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[8] + "\n*Negative:* " + percent[9] + "\n" + average[4] + "\n\n*Fulfillment*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#FF8402",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*Positive:* " + percent[10] + "\n*Negative:* " + percent[11] + "\n" + average[5] + "\n\n*Overall*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "color": "#02FF57",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": average[6]
-                    }
-                }
-            ]
-        };
-
-        // Replying
-        bot.reply(message, content);
-    });
-
-    function getPercentage(user) {
-        controller.storage.results.find({team: user.team}, function(error, results) {
+        controller.storage.results.find({ team: message.team }, function(error, results) {
             var arrayLength = results.length;
             for (var i; i < arrayLength; i++) {
-                // Housekeeping variables
+                // Results variables
                 var instance = results[i];
                 var checkIn = instance.checkin;
                 var checkOut = instance.checkout;
+                var checkInScore = checkIn[4];
+                var checkOutScore = checkOut[4];
+                var scoreDifference = checkInScore - checkOutScore;
     
+                // Necessary constants
+                var positiveDay;
+                var negativeDay;
+                // Checkin Constants
+                var checkInSleep;
+                var checkInEnergy;
+                var checkInMood;
+                var checkInMotivation;
+                // Checkout Constants
+                var checkOutEfficiency;
+                var checkOutEnergy;
+                var checkOutMood;
+                var checkOutFulfillment;
+    
+                // Check In
+                checkInSleep = checkInSleep + checkIn[0];
+                checkInEnergy = checkInEnergy + checkIn[1];
+                checkInMood = checkInMood + checkIn[2];
+                checkInMotivation = checkInMotivation + checkIn[3];
+    
+                // Check Out
+                checkOutEfficiency = checkOutEfficiency + checkOut[0];
+                checkOutEnergy = checkOutEnergy + checkOut[1];
+                checkOutMood = checkOutMood + checkOut[2];
+                checkOutFulfillment = checkOutFulfillment + checkOut[3];
+    
+                // Determine Day Outcome
+                if (scoreDifference >= 0) {
+                    positiveDay = positiveDay + 1;
+                } else {
+                    negativeDay = negativeDay + 1;
+                }
+
+                // Percent
                 // Necessary variables
                 var sleepPositive;
                 var sleepNegative;
@@ -166,8 +104,9 @@ module.exports = function(controller) {
                 } else {
                     fulfillmentNegative = fulfillmentNegative + 1;
                 }
-            }
     
+            }
+
             var sleepPositiveOutcome = (sleepPositive / arrayLength) * 100;
             var sleepNegativeOutcome = (sleepNegative / arrayLength)* 100;
             var energyPositiveOutcome = (energyPositive / (arrayLength * 2)) * 100;
@@ -181,59 +120,6 @@ module.exports = function(controller) {
             var fulfillmentPositiveOutcome = (fulfillmentPositive / arrayLength) * 100;
             var fulfillmentNegativeOutcome = (fulfillmentNegative / arrayLength) * 100;
 
-            var percentArray = [sleepPositiveOutcome, sleepNegativeOutcome, energyPositiveOutcome, energyNegativeOutcome, moodPositiveOutcome, moodNegativeOutcome, motivationPositiveOutcome, motivationNegativeOutcome, efficiencyPositiveOutcome, efficiencyNegativeOutcome, fulfillmentPositiveOutcome, fulfillmentNegativeOutcome];
-    
-            return percentArray;
-        });
-    }
-
-    function getAverage(user) {
-        controller.storage.results.find({ team: user.team }, function(error, results) {
-            var arrayLength = results.length;
-            for (var i; i < arrayLength; i++) {
-                // Results variables
-                var instance = results[i];
-                var checkIn = instance.checkin;
-                var checkOut = instance.checkout;
-                var checkInScore = checkIn[4];
-                var checkOutScore = checkOut[4];
-                var scoreDifference = checkInScore - checkOutScore;
-    
-                // Necessary constants
-                var positiveDay;
-                var negativeDay;
-                // Checkin Constants
-                var checkInSleep;
-                var checkInEnergy;
-                var checkInMood;
-                var checkInMotivation;
-                // Checkout Constants
-                var checkOutEfficiency;
-                var checkOutEnergy;
-                var checkOutMood;
-                var checkOutFulfillment;
-    
-                // Check In
-                checkInSleep = checkInSleep + checkIn[0];
-                checkInEnergy = checkInEnergy + checkIn[1];
-                checkInMood = checkInMood + checkIn[2];
-                checkInMotivation = checkInMotivation + checkIn[3];
-    
-                // Check Out
-                checkOutEfficiency = checkOutEfficiency + checkOut[0];
-                checkOutEnergy = checkOutEnergy + checkOut[1];
-                checkOutMood = checkOutMood + checkOut[2];
-                checkOutFulfillment = checkOutFulfillment + checkOut[3];
-    
-                // Determine Day Outcome
-                if (scoreDifference >= 0) {
-                    positiveDay = positiveDay + 1;
-                } else {
-                    negativeDay = negativeDay + 1;
-                }
-    
-            }
-    
             var sleepOutcome = checkInSleep / arrayLength;
             var energyOutcome = (checkInEnergy + checkOutEnergy) / (arrayLength * 2);
             var moodOutcome = (checkInMood + checkOutMood) / (arrayLength * 2);
@@ -282,9 +168,99 @@ module.exports = function(controller) {
             } else {
                 var dayMessage = 'The average employee\'s emotional state got *_better_* following the work day';
             }
-    
-            var averageArray = [sleepMessage, energyMessage, moodMessage, motivationMessage, efficiencyMessage, fulfillmentMessage, dayMessage];
-            return averageArray;
+
+        // Block Content
+        const content = {
+            blocks: [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hey! Here are your results...\n\n*Sleep*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#02D2FF",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + sleepPositiveOutcome + "\n*Negative:* " + sleepNegativeOutcome + "\n" + sleepMessage + "\n\n*Energy*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#2A02FF",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + energyPositiveOutcome + "\n*Negative:* " + energyNegativeOutcome + "\n" + energyMessage + "\n\n*Mood*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#8A02FF",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + moodPositiveOutcome + "\n*Negative:* " + moodNegativeOutcome + "\n" + moodMessage + "\n\n*Motivation*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#CF02FF",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + motivationPositiveOutcome + "\n*Negative:* " + motivationNegativeOutcome + "\n" + motivationMessage + "\n\n*Efficiency*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#FF029D",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + efficiencyPositiveOutcome + "\n*Negative:* " + efficiencyNegative + "\n" + efficiencyMessage + "\n\n*Fulfillment*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#FF8402",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Positive:* " + fulfillmentPositiveOutcome + "\n*Negative:* " + fulfillmentNegativeOutcome + "\n" + fulfillmentMessage + "\n\n*Overall*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "color": "#02FF57",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": dayMessage
+                    }
+                }
+            ]
+        };
+
+        // Replying
+        bot.reply(message, content);
         });
-    }
+    })
 }
