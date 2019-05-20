91452b26c62b1688983033138f2a3e643c8a8860
Ian Scalzo <scalzo.ian@gmail.com>
2019-05-20 11:59:43 -0400

    switched test

--- skills/test.js
+++ skills/test.js
@@ -1,95 +1,110 @@
 module.exports = function(controller) {
     controller.hears(['result', 'Result', 'results', 'Results'], 'direct_message', function(bot, message) {
-        // Percent Variables
-        var sleepPositive;
-        var sleepNegative;
-        var energyPositive;
-        var energyNegative;
-        var moodPositive;
-        var moodNegative;
-        var motivationPositive;
-        var motivationNegative;
-        var efficiencyPositive;
-        var efficiencyNegative;
-        var fulfillmentPositive;
-        var fulfillmentNegative;
+        // Necessary constants
+        var positiveDay;
+        var negativeDay;
+        var tally;
+        // Checkin Constants
+        var checkInSleep;
+        var checkInEnergy;
+        var checkInMood;
+        var checkInMotivation;
+        // Checkout Constants
+        var checkOutEfficiency;
+        var checkOutEnergy;
+        var checkOutMood;
+        var checkOutFulfillment;
 
-        var results = controller.storage.results.find({team: message.team});
-        console.log("RESULTS: ", results);
-        var arrayLength = results.length;
-        console.log("LENGTH: " + arrayLength);
-        for (var i; i < arrayLength; i++) {
-            // Results variables
-            var instance = results[i];
-            console.log("INSTANCE: ", instance);
-            var checkIn = instance.checkin;
-            console.log("CHECK IN: ", checkIn);
-            var checkOut = instance.checkout;
-            console.log("CHECK OUT: ", checkOut);
-
-            // Check In
-            if (checkIn[0] < 2) {
-                sleepPositive = sleepPositive + 1;
-            } else {
-                sleepNegative = sleepNegative + 1;
-            }
-            if (checkIn[1] < 2) {
-            energyPositive = energyPositive + 1;
-            } else {
-                energyNegative = energyNegative + 1;
-            }
-            if (checkIn[2] < 2) {
-                moodPositive = moodPositive + 1;
-            } else {
-                moodNegative = moodNegative + 1;
-            }
-            if (checkIn[3] < 2) {
-                motivationPositive = motivationPositive + 1;
-            } else {
-                motivationNegative = motivationNegative + 1;
-            }
+        controller.storage.results.find({ team: user.team }, function(error, results) {
+            var arrayLength = results.length;
+            console.log("LENGTH: " + arrayLength);
+            for (var i; i < arrayLength; i++) {
+                // Results variables
+                var instance = results[i];
+                console.log("INSTANCE: ", instance);
+                var checkIn = instance.checkin;
+                console.log("CHECKIN: ", checkIn);
+                var checkOut = instance.checkout;
+                console.log("CHECKOUT: ", checkOut);
+                var checkInScore = checkIn[4];
+                var checkOutScore = checkOut[4];
+                var scoreDifference = checkInScore - checkOutScore;
     
-            // Check Out
-            if (checkOut[0] < 2) {
-                efficiencyPositive = efficiencyPositive + 1;
-            } else {
-                efficiencyNegative = efficiencyNegative + 1;
+                // Check In
+                checkInSleep = checkInSleep + checkIn[0];
+                checkInEnergy = checkInEnergy + checkIn[1];
+                checkInMood = checkInMood + checkIn[2];
+                checkInMotivation = checkInMotivation + checkIn[3];
+    
+                // Check Out
+                checkOutEfficiency = checkOutEfficiency + checkOut[0];
+                checkOutEnergy = checkOutEnergy + checkOut[1];
+                checkOutMood = checkOutMood + checkOut[2];
+                checkOutFulfillment = checkOutFulfillment + checkOut[3];
+    
+                // Determine Day Outcome
+                if (scoreDifference >= 0) {
+                    positiveDay = positiveDay + 1;
+                    tally = tally + 1;
+                } else {
+                    negativeDay = negativeDay + 1;
+                    tally = tally + 1;
+                }
+    
             }
-            if (checkOut[1] < 2) {
-                energyPositive = energyPositive + 1;
-            } else {
-                energyNegative = energyNegative + 1;
-            }
-            if (checkOut[2] < 2) {
-                moodPositive = moodPositive + 1;
-            } else {
-                moodNegative = moodNegative + 1;
-            }
-            if (checkOut[3] < 2) {
-                fulfillmentPositive = fulfillmentPositive + 1;
-            } else {
-                fulfillmentNegative = fulfillmentNegative + 1;
-            }
+        })
+    
+        var sleepOutcome = checkInSleep / tally;
+        var energyOutcome = (checkInEnergy + checkOutEnergy) / (tally * 2);
+        var moodOutcome = (checkInMood + checkOutMood) / (tally * 2);
+        var motivationOutcome = checkInMotivation / tally;
+        var efficiencyOutcome = checkOutEfficiency / tally;
+        var fulfillmentOutcome = checkOutFulfillment / tally;
+    
+        if (sleepOutcome > 2) {
+            var sleepMessage = '*Average*: Negative';
+        } else {
+            var sleepMessage = '*Average*: Positive';
         }
+    
+        if (energyOutcome > 2) {
+            var energyMessage = '*Average*: Negative';
+        } else {
+            var energyMessage = '*Average*: Positive';
+        }
+    
+        if (moodOutcome > 2) {
+            var moodMessage = '*Average*: Negative';
+        } else {
+            var moodMessage = '*Average*: Positive';
+        }
+    
+        if (motivationOutcome > 2) {
+            var motivationMessage = '*Average*: Negative';
+        } else {
+            var motivationMessage = '*Average*: Positive';
+        }
+    
+        if (efficiencyOutcome > 2) {
+            var efficiencyMessage = '*Average*: Negative';
+        } else {
+            var efficiencyMessage = '*Average*: Positive';
+        }
+    
+        if (fulfillmentOutcome > 2) {
+            var fulfillmentMessage = '*Average*: Negative';
+        } else {
+            var fulfillmentMessage = '*Average*: Positive';
+        }
+    
+        if (positiveDay > negativeDay) {
+            var dayMessage = 'The average employee\'s emotional state got *_worse_* following the work day';
+        } else {
+            var dayMessage = 'The average employee\'s emotional state got *_better_* following the work day';
+        }
+    
+        var averageArray = [sleepMessage, energyMessage, moodMessage, motivationMessage, efficiencyMessage, fulfillmentMessage, dayMessage];
+        bot.reply(message, "Average: " + averageArray[4]);  
 
-        var tally = sleepNegative + sleepPositive;
-        var sleepPositiveOutcome = (sleepPositive / tally) * 100;
-        var sleepNegativeOutcome = (sleepNegative / tally)* 100;
-        var energyPositiveOutcome = (energyPositive / (tally * 2)) * 100;
-        var energyNegativeOutcome = (energyNegative / (tally * 2)) * 100;
-        var moodPositiveOutcome = (moodPositive / (tally * 2)) * 100;
-        var moodNegativeOutcome = (moodNegative / (tally * 2)) * 100;
-        var motivationPositiveOutcome = (motivationPositive / tally) * 100;
-        var motivationNegativeOutcome = (motivationNegative / tally) * 100;
-        var efficiencyPositiveOutcome = (efficiencyPositive / tally) * 100;
-        var efficiencyNegativeOutcome = (efficiencyNegative / tally) * 100;
-        var fulfillmentPositiveOutcome = (fulfillmentPositive / tally) * 100;
-        var fulfillmentNegativeOutcome = (fulfillmentNegative / tally) * 100;
-
-        var percent = [sleepPositiveOutcome, sleepNegativeOutcome, energyPositiveOutcome, energyNegativeOutcome, moodPositiveOutcome, moodNegativeOutcome, motivationPositiveOutcome, motivationNegativeOutcome, efficiencyPositiveOutcome, efficiencyNegativeOutcome, fulfillmentPositiveOutcome, fulfillmentNegativeOutcome];
-        bot.reply(message, "*Sleep*\nPositive: " + percent[0] + "%\nNegative: " + percent[1] + "%");
-
-        
-
-    });
+    })
 }
