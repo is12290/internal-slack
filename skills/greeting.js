32cf65418bd69d68887bf5f9e4e4937c931a0b02
Ian Scalzo <scalzo.ian@gmail.com>
2019-05-24 12:27:54 -0400

    type

--- skills/greeting.js
+++ skills/greeting.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
 
     controller.hears(['^hi', '^help', '^info', '^hello', '^yo'], 'direct_message,direct_mention', function(bot, message) {
-        bot.reply(message, "Hi there! \nI'm not the greatest at human conversation, but I'm somehow pretty emotionally intelligent.\n\nIt'd be greatly appreciated if you used `Check in` or `Check out` to log how you're doing. \n Also, use `Results` to get an insight into your organization's overall emotional fitness for the day or `Weekly Results` for the week\n\nEmail us at support@getinternal.co if you need further assistance!")
+        bot.reply(message, "Hi there! \nI'm not the greatest at human conversation, but I'm somehow pretty emotionally intelligent.\n\nIt'd be greatly appreciated if you used `Check in` or `Check out` to log how you're doing. \n Also, use `Results` to get an insight into your organization's overall emotional fitness for the day or `Weekly Results` for the week. Don't forget about `Personal Results`, as well, to check out your own emotional fitness scores for the week! \n\nEmail us at support@getinternal.co if you need further assistance!")
     });
 
 
