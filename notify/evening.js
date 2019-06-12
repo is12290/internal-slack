b8f771ffc7d096e7deab1530113a31ca2af31916
Ian Scalzo <scalzo.ian@gmail.com>
2019-06-12 18:01:18 -0400

    yikes, changed some notify things

--- notify/evening.js
+++ notify/evening.js
@@ -1,39 +1,56 @@
-module.exports = function (controller) {
-    controller.storage.teams.all(function (err, all_teams) {
+const dotenv = require('dotenv');
+dotenv.config();
+const Botkit = require('botkit');
 
-        console.log('ALL TEAMS: ', all_teams);
-        // Check out
-        var clean_data = [];
-  
-        for (var i = 0; i < all_teams.length; i++) {
-            var instance = all_teams[i];
-            if (!instance.channel) {
-                //pass
+var bot_options = {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    clientSigningSecret: process.env.clientSigningSecret,
+    // debug: true,
+    scopes: ['bot'],
+    studio_token: process.env.studio_token,
+    studio_command_uri: process.env.studio_command_uri
+};
+
+var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGODB_URI, useNewUrlParser: true, tables: ['results', 'week', 'personal', 'full']});
+bot_options.storage = mongoStorage;
+
+var controller = Botkit.slackbot(bot_options);
+
+controller.storage.teams.all(function (err, all_teams) {
+
+    console.log('ALL TEAMS: ', all_teams);
+    // Check out
+    var clean_data = [];
+
+    for (var i = 0; i < all_teams.length; i++) {
+        var instance = all_teams[i];
+        if (!instance.channel) {
+            //pass
+        } else {
+            clean_data.push([instance.bot.token, instance.channel]);
+        }
+
+    }
+    console.log('CLEAN DATA: ', clean_data);
+
+    for (var i = 0; i < clean_data.length; i++) {
+        controller.spawn({ token: clean_data[i][0] }, function (bot) {
+            var d = new Date();
+            var n = d.getDay();
+
+            if (n === 5) {
+                bot.say({
+                    text: 'Way to make it through the week! Remember to check out with me and view your organization\'s emotional fitness for the week with `Weekly Results`',
+                    channel: clean_data[i][1]
+                });
             } else {
-                clean_data.push([instance.bot.token, instance.channel]);
+                bot.say({
+                    text: 'Good afternoon! Don\'t forget to check out with me *and* report daily results today! (Send me `Check Out` or `Daily Results` over DM)',
+                    channel: clean_data[i][1]
+                });
             }
-  
-        }
-        console.log('CLEAN DATA: ', clean_data);
-  
-        for (var i = 0; i < clean_data.length; i++) {
-            controller.spawn({ token: clean_data[i][0] }, function (bot) {
-                var d = new Date();
-                var n = d.getDay();
-  
-                if (n === 5) {
-                    bot.say({
-                        text: 'Way to make it through the week! Remember to check out with me and view your organization\'s emotional fitness for the week with `Weekly Results`',
-                        channel: clean_data[i][1]
-                    });
-                } else {
-                    bot.say({
-                        text: 'Good afternoon! Don\'t forget to check out with me *and* report daily results today! (Send me `Check Out` or `Daily Results` over DM)',
-                        channel: clean_data[i][1]
-                    });
-                }
-  
-            });
-        }
-    });
-}
+
+        });
+    }
+});
--- notify/morning.js
+++ notify/morning.js
@@ -1,36 +1,52 @@
-module.exports = function (controller) {
-    // Morning
-    controller.storage.teams.all(function (err, all_teams) {
-        // Check in
-        var no_data = [];
-        var clean_data = [];
+const dotenv = require('dotenv');
+dotenv.config();
+const Botkit = require('botkit');
 
-        for (var i = 0; i < all_teams.length; i++) {
-            var instance = all_teams[i];
-            if (!instance.channel) {
-                no_data.push([instance.bot.token, instance.createdBy])
-            } else {
-                clean_data.push([instance.bot.token, instance.channel]);
-            }
+var bot_options = {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    clientSigningSecret: process.env.clientSigningSecret,
+    // debug: true,
+    scopes: ['bot'],
+    studio_token: process.env.studio_token,
+    studio_command_uri: process.env.studio_command_uri
+};
 
+var mongoStorage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGODB_URI, useNewUrlParser: true, tables: ['results', 'week', 'personal', 'full'] });
+bot_options.storage = mongoStorage;
+
+var controller = Botkit.slackbot(bot_options);
+// Morning
+controller.storage.teams.all(function (err, all_teams) {
+    // Check in
+    var no_data = [];
+    var clean_data = [];
+
+    for (var i = 0; i < all_teams.length; i++) {
+        var instance = all_teams[i];
+        if (!instance.channel) {
+            no_data.push([instance.bot.token, instance.createdBy])
+        } else {
+            clean_data.push([instance.bot.token, instance.channel]);
         }
 
-        for (var i = 0; i < clean_data.length; i++) {
-            controller.spawn({ token: clean_data[i][0] }, function (bot) {
-                bot.say({
-                    text: 'Good morning! Don\'t forget to check in with @internal today! (Send me `Check In` over DM)',
-                    channel: clean_data[i][1]
-                });
+    }
+
+    for (var i = 0; i < clean_data.length; i++) {
+        controller.spawn({ token: clean_data[i][0] }, function (bot) {
+            bot.say({
+                text: 'Good morning! Don\'t forget to check in with @internal today! (Send me `Check In` over DM)',
+                channel: clean_data[i][1]
             });
-        }
+        });
+    }
 
-        for (var z = 0; z < no_data.length; z++) {
-            controller.spawn({ token: no_data[i][0] }, function (bot) {
-                bot.say({
-                    text: 'Add me to a company wide channel so that I can send daily reminders to fill out logs! You can do this by mentioning me in the channel, or clicking \'Show Channel Details\' -> \'App\' -> \'Add app\'',
-                    user: no_data[i][1]
-                });
+    for (var z = 0; z < no_data.length; z++) {
+        controller.spawn({ token: no_data[i][0] }, function (bot) {
+            bot.say({
+                text: 'Add me to a company wide channel so that I can send daily reminders to fill out logs! You can do this by mentioning me in the channel, or clicking \'Show Channel Details\' -> \'App\' -> \'Add app\'',
+                user: no_data[i][1]
             });
-        }
-    });
-}
+        });
+    }
+});
