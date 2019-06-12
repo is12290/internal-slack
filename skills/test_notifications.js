0147f4d534643cbb36d105930ea71a96e5ea2af6
Ian Scalzo <scalzo.ian@gmail.com>
2019-06-12 14:04:25 -0400

    fixed test notification

--- skills/test_notifications.js
+++ skills/test_notifications.js
@@ -3,7 +3,6 @@ module.exports = function (controller) {
     var no_data = [];
     var clean_data = [];
     controller.storage.teams.all(function (err, all_teams) {
-        console.log(all_teams)
 
         for (var i = 0; i < all_teams.length; i++) {
             var instance = all_teams[i];
@@ -19,7 +18,7 @@ module.exports = function (controller) {
     console.log('CLEAN DATA: ', clean_data);
     console.log('NO DATA: ', no_data);
 
-    controller.spawn({ token: clean_data[1][0] }, function (err, bot) {
+    controller.spawn({ token: clean_data[0][0] }, function (err, bot) {
         bot.say({
             text: 'haeyy!',
             channel: clean_data[1][1]
