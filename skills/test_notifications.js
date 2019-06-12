2ba25872f5f7a0a2af3bd41f546240f48e7ce3ae
Ian Scalzo <scalzo.ian@gmail.com>
2019-06-12 15:10:32 -0400

    added cron scheduling - heroku scheduling sucks

--- skills/test_notifications.js
+++ skills/test_notifications.js
@@ -1,29 +1,31 @@
 module.exports = function (controller) {
-    controller.storage.teams.all(function (err, all_teams) {
-        //test
-        var no_data = [];
-        var clean_data = [];
+    const schedule = require('node-schedule-tz');
 
-        for (var i = 0; i < all_teams.length; i++) {
-            var instance = all_teams[i];
-            if (!instance.channel) {
-                no_data.push([instance.bot.token, instance.createdBy])
-            } else {
-                clean_data.push([instance.bot.token, instance.channel]);
+    var test = schedule.scheduleJob('*/5 * * * *', 'America/New_York', function () {
+        controller.storage.teams.all(function (err, all_teams) {
+            //test
+            var no_data = [];
+            var clean_data = [];
+
+            for (var i = 0; i < all_teams.length; i++) {
+                var instance = all_teams[i];
+                if (!instance.channel) {
+                    no_data.push([instance.bot.token, instance.createdBy])
+                } else {
+                    clean_data.push([instance.bot.token, instance.channel]);
+                }
+
             }
 
-        }
+            console.log('CLEAN DATA: ', clean_data);
+            console.log('NO DATA: ', no_data);
 
-        console.log('CLEAN DATA: ', clean_data);
-        console.log('NO DATA: ', no_data);
-
-        controller.spawn({ token: clean_data[0][0] }, function (bot) {
-            bot.say({
-                text: 'haeyy!',
-                channel: clean_data[0][1]
+            controller.spawn({ token: clean_data[0][0] }, function (bot) {
+                bot.say({
+                    text: 'haeyy!',
+                    channel: clean_data[0][1]
+                });
             });
         });
     });
-
-
 }
