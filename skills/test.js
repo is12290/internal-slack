ff97027220406873d00af0da8e0983aaac31f90a
Ian Scalzo <ian@current.si>
2019-07-02 22:35:16 -0400

    fixed test

--- skills/test.js
+++ skills/test.js
@@ -1,5 +1,5 @@
 module.exports = function (controller) {
-    controller.hears(['test'], 'direct_message', function (message, bot) {
+    controller.hears(['test'], 'direct_message', function (bot, message) {
         controller.storage.teams.get(message.team, function (err, info) {
             bot.reply(message, info);
         })
