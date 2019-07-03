64d9ce9ca9bf7962a625821c57eb1d1b1f5c7136
Ian Scalzo <ian@current.si>
2019-07-02 22:42:07 -0400

    logging

--- skills/test.js
+++ skills/test.js
@@ -1,7 +1,12 @@
 module.exports = function (controller) {
     controller.hears(['test'], 'direct_message', function (bot, message) {
+        console.log(message.team);
         controller.storage.teams.get(message.team, function (err, info) {
-            bot.reply(message, info);
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(info);
+            }
         })
     })
 }
