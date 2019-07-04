344307d5ec9c8d3ef121abf9fdc916c19e22e737
Ian Scalzo <ian@current.si>
2019-07-04 18:49:43 -0400

    delete question ready to go

--- skills/delete_question.js
+++ skills/delete_question.js
@@ -2,9 +2,10 @@ module.exports = function (controller) {
     controller.hears(['^delete question', '^delete Question', '^delete custom question', '^delete Custom Question'], 'direct_message,direct_mention', function (bot, message) {
         controller.storage.teams.get(message.team, function (err, info) {
             if (!info || typeof info.customization == 'undefined' || typeof info.customization.question == 'undefined') {
-                bot.reply(message, "I'm sorry, there's no custom question for me to delete!\nIf I'm wrong, please tell my human counterparts at support@getinternal.co");
+                bot.reply(message, "I'm sorry, there's no custom question for me to delete!\n\nIf I'm wrong, please tell my human counterparts at support@getinternal.co");
             } else if (typeof info.customization.question != 'undefined') {
                 bot.startConversation(message, function (err, convo){
+                    var outcome = [];
                     convo.addQuestion({
                         attachments: [{
                                 text: "The current question you have set is: '" + info.customization.question.topic + "', are you sure you want to delete it?",
@@ -55,9 +56,7 @@ module.exports = function (controller) {
                                         }]
                                     }
                                 );
-                                delete info.customization.question;
-                                controller.storage.teams.save(info);
-                                bot.reply(response, "You got it! Question has been removed.")
+                                outcome.push(1)
                             }
                         },
                         {
@@ -87,12 +86,27 @@ module.exports = function (controller) {
                                         }]
                                     }
                                 );
-                                bot.reply(response, "Well, okay then... Enjoy the rest of your day! :sunglasses:");
-
+                                outcome.push(0);
                                 
                             }
                         }
                     ]);
+
+                    convo.activate();
+
+                    convo.on('end', function (convo) {
+                        if (convo.sucessful()) {
+                            if (outcome[0] == 1) {
+                                delete info.customization.question;
+                                controller.storage.teams.save(info);
+                                bot.reply(response, "You got it! Question has been removed.")
+                            } else if( outcome[0] == 0) {
+                                bot.reply(response, "Well, okay then... Enjoy the rest of your day! :sunglasses:");
+                            }
+                        } else {
+                            bot.reply(message, "Would you mind sending me that message again?");
+                        }
+                    })
                 });
             } else {
                 bot.reply(message, "Would you mind sending me that message again?");
