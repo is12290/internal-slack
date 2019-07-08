08a415bdfd53c110105c9b3631f44288d319ed7c
Ian Scalzo <ian@current.si>
2019-07-08 11:55:35 -0400

    jesus

--- automacy/say_test.js
+++ automacy/say_test.js
@@ -22,31 +22,20 @@ controller.storage.teams.all(function (error, all_teams) {
         controller.spawn({ token: all_teams[i].bot.token }, function (bot) {
             controller.storage.users.find({ team: all_teams[i].id }, function (err, all_users) {
                 for (var j = 0; j < all_users.length; j++) {
-                    // bot.say({
-                    //     text: 'This is a test',
-                    //     channel: all_users[j].channels[0]
-                    // }, function (err, response) {
-                    //     if (err) {
-                    //         console.log("error: ", err);
-                    //     } else {
-                    //         console.log("response: ", response);
-                    //     }
-                    // });
-                    bot.startConversation({
-                        user: all_users[j].id,
-                        channel: all_users[j].id,
-                        text: 'dummy'
-                    }, function (err, convo) {
-                        convo.ask({
-                            channel: all_users[j].id,
-                            text: 'Just what do you think you are doing, Dave?'
-                        }, function (res, convo) {
-                            convo.say(res.text + ' is not a good enough answer.')
-                            convo.next()
+                    bot.say({
+                        text: 'This is a test',
+                        channel: all_users[j].channels[0]
+                    }, function (err, response) {
+                        if (err) {
+                            console.log("error: ", err);
+                        }
+                        bot.startConversation(response, function (err, convo) {
+                            convo.ask("PLEASE WORK");
+                            
                         }
                         )
-                    }
-                    )
+                    });
+                    
                 }
             })
         });
