24cf3fa1380a9e6d10b805a527efbcd61a536e7f
Ian Scalzo <ian@current.si>
2019-07-08 11:52:16 -0400

    fuckin hell

--- automacy/say_test.js
+++ automacy/say_test.js
@@ -38,7 +38,7 @@ controller.storage.teams.all(function (error, all_teams) {
                         text: 'dummy'
                     }, function (err, convo) {
                         convo.ask({
-                            channel: all_users[j].channels[0],
+                            channel: all_users[j].id,
                             text: 'Just what do you think you are doing, Dave?'
                         }, function (res, convo) {
                             convo.say(res.text + ' is not a good enough answer.')
