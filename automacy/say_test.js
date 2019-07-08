07933af3a2a7be54ea9810d4a8fb3f5c543b8f54
Ian Scalzo <ian@current.si>
2019-07-08 11:47:34 -0400

    test test test

--- automacy/say_test.js
+++ automacy/say_test.js
@@ -18,22 +18,37 @@ bot_options.storage = mongoStorage;
 var controller = Botkit.slackbot(bot_options);
 
 controller.storage.teams.all(function (error, all_teams) {
-  for (var i = 0; i < all_teams.length; i++) {
-    controller.spawn( {token: all_teams[i].bot.token}, function (bot) {
-        controller.storage.users.find( { team: all_teams[i].id }, function (err, all_users) {
-            for (var j = 0; j < all_users.length; j++) {
-                bot.say({
-                    text: 'This is a test',
-                    channel: all_users[j].channels[0]
-                }, function (err, response) {
-                    if (err) {
-                        console.log("error: ", err);
-                    } else {
-                        console.log("response: ", response);
+    for (var i = 0; i < all_teams.length; i++) {
+        controller.spawn({ token: all_teams[i].bot.token }, function (bot) {
+            controller.storage.users.find({ team: all_teams[i].id }, function (err, all_users) {
+                for (var j = 0; j < all_users.length; j++) {
+                    // bot.say({
+                    //     text: 'This is a test',
+                    //     channel: all_users[j].channels[0]
+                    // }, function (err, response) {
+                    //     if (err) {
+                    //         console.log("error: ", err);
+                    //     } else {
+                    //         console.log("response: ", response);
+                    //     }
+                    // });
+                    bot.startConversation({
+                        user: all_users[j].id,
+                        channel: all_users[j].channels[0],
+                        text: 'dummy'
+                    }, function (err, convo) {
+                        convo.ask({
+                            channel: all_users[j].channels[0],
+                            text: 'Just what do you think you are doing, Dave?'
+                        }, function (res, convo) {
+                            convo.say(res.text + ' is not a good enough answer.')
+                            convo.next()
+                        }
+                        )
                     }
-                });
-            }
-        })
-    })
-  }
-});
+                    )
+                }
+            })
+        });
+    }
+})
