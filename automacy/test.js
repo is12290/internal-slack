5f11027978b044704428bc915b8d06da645c5d2e
Ian Scalzo <ian@current.si>
2019-07-07 22:27:29 -0400

    Maybe this test will work?

--- automacy/test.js
+++ automacy/test.js
@@ -12,8 +12,30 @@ var bot_options = {
   studio_command_uri: process.env.studio_command_uri
 };
 
-var mongoStorage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGODB_URI, useNewUrlParser: true, tables: ['results', 'week', 'personal', 'full'] });
+var mongoStorage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGODB_URI, useNewUrlParser: true, tables: ['teams', 'user'] });
 bot_options.storage = mongoStorage;
 
 var controller = Botkit.slackbot(bot_options);
 
+controller.storage.teams.all(function (error, all_teams) {
+  for (var team in all_teams) {
+    controller.spawn( {token: team.bot.token}, function (bot) {
+      controller.storage.users.find( {team: team}, function (error, results) {
+        for (var result in results) {
+          bot.startConversation({user: result.id, channel: result.channels[0]}, function (err, convo) {
+            if (err) {
+              console.log("ERROR: ", err);
+            } else {
+              convo.say("Hey!");
+              convo.next();
+              convo.addQuestion("How are you?", function (response, convo) {
+                convo.say("cool, you said: " + response.text);
+                convo.next();
+              }, {}, 'default');
+            }
+          })
+        }
+      })
+    })
+  }
+});
