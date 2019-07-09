4b978df5b1ebc9063c020169d93b8c7fcbaef56b
Ian Scalzo <ian@current.si>
2019-07-09 15:29:56 -0400

    vleh

--- skills/test.js
+++ skills/test.js
@@ -1,9 +1,6 @@
 module.exports = function (controller) {
-    controller.hears(['test'], 'direct_mention', function (bot, message) {
-        console.log(message.user);
-        bot.startPrivateConversation({user: message.user}, function (err, convo) {
-            console.log(convo);
-            convo.say("this work?");
-        })
+    controller.on('slash_command', function (bot, message) {
+        console.log(message);
+        console.log(message.command);
     })
 }
