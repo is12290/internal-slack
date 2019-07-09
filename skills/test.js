b5dbe2215789b405d739fee494bf95b3bc80a1c3
Ian Scalzo <ian@current.si>
2019-07-08 23:19:10 -0400

    jesus

--- skills/test.js
+++ skills/test.js
@@ -2,6 +2,7 @@ module.exports = function (controller) {
     controller.hears(['test'], 'direct_mention', function (bot, message) {
         console.log(message.user);
         bot.startPrivateConversation({user: message.user}, function (err, convo) {
+            console.log(convo);
             convo.say("this work?");
         })
     })
