17730baf21bd155ffbd4a9a8bfb7f20099295242
Ian Scalzo <ian@current.si>
2019-07-19 16:38:06 -0400

    hmm

--- automacy/check_in_logs.js
+++ automacy/check_in_logs.js
@@ -52,7 +52,7 @@ if (2+2 == 5) {  //(n === 6 || n === 0) {
         data.forEach(
             (instance) => {
                 controller.spawn({ token: instance[0] }, function (bot) {
-                    bot.startPrivateConversation({ user: instance[0] }, function (err, convo) {
+                    bot.startPrivateConversation({ user: user_data[0] }, function (err, convo) {
                         // Keep Score
                         const score = [];
 
