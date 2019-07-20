800cb4f46db6dc323f631405e5bab7ad14c0bd81
Ian Scalzo <ian@current.si>
2019-07-19 20:33:55 -0400

    hm

--- automacy/check_in_logs.js
+++ automacy/check_in_logs.js
@@ -38,24 +38,21 @@ if (2+2 == 5) {  //(n === 6 || n === 0) {
                 if (2+2==4){//(user.customization.logging.check_in_time == now.tz(user.customization.logging.timezone).format('HH:mm')) {
                     user_data = [];
                     controller.storage.teams.get(user.team, function (err, team_info) {
-                        console.log(team_info);
                         user_data.push(team_info.bot.token);
                     });
                     user_data.push(user.id);
                     user_data.push(user.channel);
                     data.push(user_data);
-                    console.log("USER DATA: ", user_data);
                 } else {
                     // Pass
                 }
             }
         }
 
-        console.log("DATA: ", data);
         data.forEach(
             (instance) => {
                 console.log(instance);
-                controller.spawn({ token: instance[0] }, function (bot) {
+                controller.spawn({ token: instance.bot.token }, function (bot) {
                     bot.startPrivateConversation({ user: user_data[0] }, function (err, convo) {
                         // Keep Score
                         const score = [];
