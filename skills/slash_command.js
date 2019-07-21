30ffeaba7796a02f9a376a203a9261099a04436e
Ian Scalzo <ian@current.si>
2019-07-20 23:32:54 -0400

    slash command testing

--- skills/slash_command.js
+++ skills/slash_command.js
@@ -1,6 +1,7 @@
 module.exports = function (controller) {
     controller.on('slash_command', function (bot, message) {
-        console.log(message);
-
+        bot.startPrivateConversation({user: message.user_id}, function (err, convo) {
+            convo.say("2 + 2 = 4");
+        })
     });
 }
