3f078787e3ccea19e9498e18f6a336bf9a0e3d8f
Ian Scalzo <ian@current.si>
2019-07-22 08:28:34 -0400

    gr

--- skills/test.js
+++ skills/test.js
@@ -5,7 +5,6 @@ module.exports = function (controller) {
                 console.log("error: ", err);
             }
 
-            const content 
             convo.addQuestion({
                 text: 'hey',
                 blocks: [
