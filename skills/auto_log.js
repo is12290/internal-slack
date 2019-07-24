d25b31d563b69bb4abc61d139597762f497913bf
Ian Scalzo <ian@current.si>
2019-07-24 17:40:49 -0400

    aye yai yai

--- skills/auto_log.js
+++ skills/auto_log.js
@@ -1,13 +1,6 @@
 module.exports = function (controller) {
     controller.on('interactive_message_callback', function (message, bot) {
 
-        bot.startConversation(message, function (err, convo) {
-            if (err) {
-                console.log("error: ", err);
-            }
-
-            convo.say("It worked!")
-
-        })
+        console.log(message);
     })
 }
