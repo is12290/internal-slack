65ce345b62b6712a8a6f4a82e93b9b7ea7a42be8
Ian Scalzo <ian@current.si>
2019-07-23 22:04:48 -0400

    tests

--- skills/test.js
+++ skills/test.js
@@ -1,63 +1,39 @@
 module.exports = function (controller) {
     controller.hears(['Test', 'test'], 'direct_message', function (bot, message) {
-        bot.startConversation(message, function (err, convo) {
-            if (err) {
+        for (var j = 0; j < 3; j++) {
+            var user = message.user;
+  
+            bot.startPrivateConversation({user: user }, function (err, convo) {
+              if (err) {
                 console.log("error: ", err);
-            }
-
-            var timeframe = [];
-            convo.addQuestion({
-                text: 'hey',
-                blocks: [
-                    {
-                        "type": "section",
-                        "block_id": "section1234",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "Would you be so kind as to select your custom timeframe?"
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "datepicker",
-                                "placeholder": {
-                                    "type": "plain_text",
-                                    "text": "Start Date",
-                                    "emoji": true
-                                }
-                            },
-                            {
-                                "type": "datepicker",
-                                "placeholder": {
-                                    "type": "plain_text",
-                                    "text": "End Date",
-                                    "emoji": true
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }, function (response, convo) {
-                timeframe.push(response.actions[0].selected_date);
-                
-                if (timeframe.length == 2) {
-                    convo.next();
-                } else {
-                    convo.silentRepeat();
+              }
+  
+              convo.addMessage({
+                text: 'This is a conversation!'
+              }, function (response, convo) {
+                console.log('Went through message');
+                convo.next();
+              });
+              console.log("Out of message");
+  
+              convo.addQuestion({
+                text: "How you be?"
+              }, function (response, convo) {
+                console.log("went through question");
+                convo.next();
+              });
+  
+              convo.activate();
+  
+              convo.on('end', function (convo) {
+                if (convo.sucessful()) {
+                  console.log("Success!");
                 }
-            });
-
-            
-            convo.activate();
-
-            convo.on('end', function (convo) {
-                if (convo.successful()) {
-                    console.log("Timeframe: ", timeframe);
-                }
+              });
+              console.log("Skipped convo.on('end')")
             })
-
-        })
+            console.log("Out of conversation");
+          }
+          console.log("Out of for loop");
     })
 }
