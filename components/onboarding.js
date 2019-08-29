8d637ea9f93c9a69518d0b5a44a9ff3165282ef6
Ian Scalzo <ian@current.si>
2019-08-29 16:45:42 -0400

    Added channel add to onboarding

--- components/onboarding.js
+++ components/onboarding.js
@@ -47,7 +47,24 @@ module.exports = function (controller) {
                         }
                     ]
                 }, function (response, convo) {
-                    channel_name = channel_name + response.text;
+                    bot.api.channels.create({ token: bot.config.app_token, name: channel_name }, function (err, response) {
+                        if (err) {
+                            console.log("error: ", err);
+                        }
+                        bot.api.channels.setPurpose({ token: bot.config.app_token, channel: response.channel.id, purpose: "This channel is used by the Internal app to share the daily overall emotional fitness scores of those who opt to share." }, function (err, response) {
+                            if (err) {
+                                console.log("error: ", err);
+                            }
+                        })
+                        bot.api.channels.setTopic({ token: bot.config.app_token, channel: response.channel.id, topic: "Know how your coworkers are doing today" }, function (err, response) {
+                            if (err) {
+                                console.log("error: ", err);
+                            }
+                        })
+
+                        team.bot.channel = response.channel.id;
+                        controller.storage.teams.save(team);
+                    });
                     bot.api.reactions.add({
                         name: 'thumbsup',
                         channel: response.channel,
@@ -57,30 +74,120 @@ module.exports = function (controller) {
                 });
                 convo.say("Awesome! I'll create that right now...");
 
-                convo.say("Let's run through a few quick things about me:\n\n*Logging*\nParticipating users carry out Check Ins and Reflections in direct message with me every day. As expected, Check Ins are completed at the start of the work day and Check Outs at the end. The logs are 4 topics with 4 possible answers for each - A total of 16 possible choices - and can be carried out by sending me a message saying `Check In` or `Reflect`\n\n*Reporting*\nYour personal results can be tracked on a weekly or monthly basis by simply telling me `Weekly Report` or `Monthly Report`. You can search for your historic scores, or compare your scores with historic scores, by telling me `Search` or `Compare`. Managers have the capability of viewing all scores, but only in aggregate so that no one’s scores are personally identifiable\n\n*Customization*\nI have the capability of automatically sending you Check Ins, Reflections, and Reports at the time of your choice so that you don’t have to remember to invoke them yourself. You can choose a time by direct messaging me `Customize Logs` or `Customize Reports`\n\nThat is pretty much it for basic understanding! I know it’s a bit to take in at once, so ask me for `help` at any time and I’ll provide some guidance.\n\nIf you have additional questions, comments, or problems, be sure to contact my creator at support@getinternal.co");
+                convo.say("Done! I need you to do one more thing for me, which is to add me to the channel that was just created. Would you mind navigating to the channel, then click the 'i' on top bar -> App -> Add App -> Select my name");
+
+                convo.addQuestion({
+                    attachments: [
+                        {
+                            callback_id: 'new-user',
+                            color: "#0294ff",
+                            text: "Task completed? (Yes is the only option)",
+                            attachment_type: 'default',
+                            actions: [
+                                {
+                                    'name': 'yes-button',
+                                    'value': 'Yes-one',
+                                    'text': 'Yes',
+                                    'type': 'button'
+                                },
+                                {
+                                    'name': 'yes-button',
+                                    'value': 'Yes-two',
+                                    'text': 'Yes',
+                                    'type': 'button'
+                                },
+                            ]
+                        }
+                    ]
+                }, [
+                        {
+                            pattern: 'Yes-one',
+                            callback: function (reply, convo) {
+                                bot.replyInteractive(reply,
+                                    {
+                                        attachments: [
+                                            {
+                                                callback_id: 'new-user',
+                                                color: "#0294ff",
+                                                text: "Task completed? (Yes is the only option)",
+                                                attachment_type: 'default',
+                                                actions: [
+                                                    {
+                                                        'name': 'yes-button',
+                                                        'value': 'Yes-one',
+                                                        'style': 'primary',
+                                                        'text': 'Yes',
+                                                        'type': 'button'
+                                                    },
+                                                    {
+                                                        'name': 'yes-button',
+                                                        'value': 'Yes-two',
+                                                        'text': 'Yes',
+                                                        'type': 'button'
+                                                    },
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                )
+                                bot.api.reactions.add({
+                                    name: 'thumbsup',
+                                    channel: reply.channel,
+                                    timestamp: reply.ts
+                                });
+                                convo.next()
+                            }
+                        },
+                        {
+                            pattern: 'Yes-two',
+                            callback: function (reply, convo) {
+                                bot.replyInteractive(reply,
+                                    {
+                                        attachments: [
+                                            {
+                                                callback_id: 'new-user',
+                                                color: "#0294ff",
+                                                text: "Task completed? (Yes is the only option)",
+                                                attachment_type: 'default',
+                                                actions: [
+                                                    {
+                                                        'name': 'yes-button',
+                                                        'value': 'Yes-one',
+                                                        'text': 'Yes',
+                                                        'type': 'button'
+                                                    },
+                                                    {
+                                                        'name': 'yes-button',
+                                                        'value': 'Yes-two',
+                                                        'style': 'primary',
+                                                        'text': 'Yes',
+                                                        'type': 'button'
+                                                    },
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                )
+                                bot.api.reactions.add({
+                                    name: 'thumbsup',
+                                    channel: reply.channel,
+                                    timestamp: reply.ts
+                                });
+                                convo.next()
+                            }
+                        },
+                    ]
+                );
+
+                convo.say("We're all set! Now let's run through a few quick things about me");
+
+                convo.say("\n\n*Logging*\nParticipating users carry out Check Ins and Reflections in direct message with me every day. As expected, Check Ins are completed at the start of the work day and Check Outs at the end. The logs are 4 topics with 4 possible answers for each - A total of 16 possible choices - and can be carried out by sending me a message saying `Check In` or `Reflect`\n\n*Reporting*\nYour personal results can be tracked on a weekly or monthly basis by simply telling me `Weekly Report` or `Monthly Report`. You can search for your historic scores, or compare your scores with historic scores, by telling me `Search` or `Compare`. Managers have the capability of viewing all scores, but only in aggregate so that no one’s scores are personally identifiable\n\n*Customization*\nI have the capability of automatically sending you Check Ins, Reflections, and Reports at the time of your choice so that you don’t have to remember to invoke them yourself. You can choose a time by direct messaging me `Customize Logs` or `Customize Reports`\n\nThat is pretty much it for basic understanding! I know it’s a bit to take in at once, so ask me for `help` at any time and I’ll provide some guidance.\n\nIf you have additional questions, comments, or problems, be sure to contact my creator at support@getinternal.co")
 
                 convo.activate();
 
                 convo.on('end', function (convo) {
                     if (convo.successful()) {
-                        bot.api.channels.create({ token: bot.config.app_token, name: channel_name }, function (err, response) {
-                            if (err) {
-                                console.log("error: ", err);
-                            }
-                            bot.api.channels.setPurpose({ token: bot.config.app_token, channel: response.channel.id, purpose: "This channel is used by the Internal app to share the daily overall emotional fitness scores of those who opt to share." }, function (err, response) {
-                                if (err) {
-                                    console.log("error: ", err);
-                                }
-                            })
-                            bot.api.channels.setTopic({ token: bot.config.app_token, channel: response.channel.id, topic: "Know how your coworkers are doing today" }, function (err, response) {
-                                if (err) {
-                                    console.log("error: ", err);
-                                }
-                            })
-
-                            team.bot.channel = response.channel.id;
-                            controller.storage.teams.save(team);
-                        });
+                        // Nothing
                     }
                 })
             }
