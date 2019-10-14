cae9b0d041db9220a04fc6c9ac90e34ca91ebfc1
Ian Scalzo <ian@current.si>
2019-10-14 16:28:15 -0400

    Fixed onboarding

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -1,5 +1,5 @@
 module.exports = function(controller) {
     controller.on('bot_channel_join', function(bot, message) {
-        bot.reply(message, "Thanks for adding me, friendly human!");
+        bot.reply(message, "Thanks for adding me, friendly human! You'll also need to invite your co-founders through `/invite`");
     })
 }
